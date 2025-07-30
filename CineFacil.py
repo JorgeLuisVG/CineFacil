26bbb536b7d367700fbf357ab173d2867369c7d2
JorgeLuisVG <jorgeluisvg0500@gmail.com>
2025-07-29 19:46:06 -0600

    funcion para la pelicula y horario

---
+++ CineFacil.py
@@ -0,0 +1,19 @@
+def Funciones():
+    print("Funciones")
+    print("1 = The Dark Knight")
+    print("2 = El Hombre Araña")
+    print("3 = Interstellar")
+    print("4 = Contacto Sangriento")
+    print("5 = Rambo")
+    print("6 = Rocky")
+    Funcion = input()
+
+    print("Horarios")
+    print("1 = 10:00 am")
+    print("2 = 2:00 pm")
+    print("3 = 4:00 pm")
+    print("4 = 6:00 pm")
+    print("5 = 8:00 pm")
+    print("6 = 10:00 pm")
+    Horario = input()
+
